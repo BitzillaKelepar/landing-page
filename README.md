83255005dd05dc63d61f71d93b13d381fb294655
Bitzilla | Kelepar <lucaraneri@hotmail.com>
2023-05-16 12:18:41 +0800

    Added files
    Created the repository.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Description](#Description)
+
+## Description
+
+The code in this repository represents the result of the landing page project from the Udacity Front Ent Web Developer nanodegree.
+In this project, the task is to modify a static landing page into a dynamic & interactive landing page with JavaScript.
+
+The following requirements are set:
+
+- Dynamically create the navigation bar including the links according to the content in the HTML file.
+- When a section is in the user's viewport, it changes to an active state.
+- Ensure a smooth scrolling behavior. 
+
+Udacity's starter code that represents the static landing page to begin with was taken from this [repository](https://github.com/udacity/fend/tree/refresh-2019/projects/landing-page).
---
+++ js/app.js
@@ -0,0 +1,101 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const menu = document.getElementById("navbar__list");
+const sections = Array.from(document.querySelectorAll("section"));
+let section;
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+// Check if section is in viewport
+function isInViewport(s) {
+    const view = s.getBoundingClientRect();
+    return (view.top <= 150 && view.bottom >= 150);
+}
+
+// Check if section is active
+function isActive(s) {
+    return s.classList.contains("active");
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// Create Menu: For each section a list item is created dynamically
+function createMenu() {
+    for (section of sections) {
+        const sectionName = section.getAttribute("data-nav");
+        const sectionId = section.getAttribute("id");
+        const navItem = document.createElement("li");
+        navItem.innerHTML = `<a class="menu__link" href="#${sectionId}">${sectionName}</a>`;
+        menu.appendChild(navItem);
+    }
+}
+
+// Add class 'active' to section when it is near top of viewport
+// Make section active and remove active
+function makeSectionActive() {
+    for (section of sections) {
+        if (isInViewport(section) && isActive(section) === false) {
+            section.classList.add("active");
+        } else if (isInViewport(section) === false && isActive(section)) {
+            section.classList.remove("active");
+        }
+    }
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu 
+createMenu();
+
+// Scroll smoothly to section on link click
+const anchor = document.querySelectorAll('a[href^="#"]');
+anchor.forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        const scroll = document.querySelector(this.getAttribute("href"));
+        scroll.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+// Set sections as active
+document.addEventListener("scroll", makeSectionActive);
+
+
+
+
